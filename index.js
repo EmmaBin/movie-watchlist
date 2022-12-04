51845bb95b00b78b9d843449426e3ae8a11c00e7
emmacodes <binma.emma@gmail.com>
2022-12-03 22:43:02 -0800

    fetch ten movie objects

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ searchForm.addEventListener('submit',(e)=>{
    e.preventDefault()
     exploreIcon.style.display = 'none'
     let searchName = document.getElementById('search-input').value
+    let movieList=[]
    
     fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=6829f1aa&s=${searchName}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        data.Search.map(movie=> movieList.push(movie.imdbID))
+        for (let movie of movieList){
+            fetch(`http://www.omdbapi.com/?i=${movie}&apikey=6829f1aa&t=${searchName}`)
+            .then(res => res.json())
+            .then(data =>{
+                console.log(data)
+            })
+        }
+        
+    })
  
 })
